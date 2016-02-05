b0ff97a11ac0232c8260d281f0671b73a472ef07
Michael Abrahamsen <mail@michaelabrahamsen.com>
2016-02-04 21:14:06 -0700

    display water volumes

--- app/pages/page2/recipe-view.js
+++ app/pages/page2/recipe-view.js
@@ -1,6 +1,7 @@
 import {Page, NavController, NavParams} from 'ionic/ionic';
 import { Http, Headers, HTTP_PROVIDERS } from 'angular2/http';
 
+
 @Page({
   templateUrl: 'build/pages/page2/recipe-view.html'
 })
@@ -13,12 +14,34 @@ export class RecipeView{
     this.hops = [];
     this.getGrains();
     this.getHops();
+
+    this.totalVol = 0;
+    this.mashVol = 0;
+    this.spargeVol = 0;
+
+    this.details = {
+      batchSize: 5,
+      boilTime: 60,
+      mashThickness: 1.33
+    };
+
+    this.equipmentProfile = {
+      name: "default profile",
+      trubLoss: 0.5,
+      equipmentLoss: 1,
+      fermenterLoss: 0,
+    };
+
+    this.grainBill = 0;
+    this.grainTemp = 65;
   }
   grainTotal = function(){
     let total = 0;
     for (var grain of this.grains){
       total += grain.amount;
     }
+    this.calculateWaterVol(total);
+    this.grainBill = total;
     return total;
   }
   getGrains = function(){
@@ -50,4 +73,50 @@ export class RecipeView{
           err => this.logError(err),
           () => console.log(this.hops);
           );
+  }
+
+  calculateWaterVol = function(grainBill){
+    var batchSize = this.details.batchSize;
+    var bt = this.details.boilTime;
+    var trubLoss = this.equipmentProfile.trubLoss;
+    let equipmentLoss = this.equipmentProfile.equipmentLoss;
+    var mashThickness = this.details.mashThickness;
+
+    var ga = grainAbsorbtion(grainBill);
+    var pv = preBoilVol(bt, batchSize, trubLoss);
+    var tv = totalVol(pv, ga, equipmentLoss);
+    var mv = mashVol(mashThickness, grainBill);
+    var sv = spargeVol(tv, mv);
+
+    this.totalVol = Math.round(tv*100) / 100;
+    this.mashVol = Math.round(mv*100) / 100;
+    this.spargeVol = Math.round(sv*100) / 100;
+  };
+  calculateStrikeTemp = function(targetTemp){
+    var strikeTemp = (0.2 / this.details.mashThickness)*(targetTemp - this.grainTemp) + targetTemp;
+    return Math.round(strikeTemp*10) / 10;
+  };
+  function grainAbsorbtion(grainBill){
+    return 0.13 * grainBill;
+  }
+  function preBoilVol(boilTime, batchSize, trubLoss){
+    var wsFactor = shrinkageFactor(0.04);
+    var ev = evaporateFactor(boilTime);
+    return ((batchSize + trubLoss) / wsFactor) / ev;
+  }
+  function evaporateFactor(boilTime){
+    return 1-(0.10 * (boilTime / 60));
+  }
+  function shrinkageFactor(percent){
+    return 1-percent;
+  }
+  function totalVol(preBoilVol, grainAbsorbtion, equipmentLoss){
+    return preBoilVol + grainAbsorbtion + equipmentLoss;
+  }
+  function mashVol(mashThickness, grainBill){
+
+    return mashThickness * grainBill * 0.25;
+  }
+  function spargeVol(totalVol, mashVol){
+    return totalVol - mashVol;
   }
